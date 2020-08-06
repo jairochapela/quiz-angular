03b34d56ba27820c4105dc07f9f998f4401adc13
Jairo Chapela-Martinez <jairochapela@gmail.com>
2020-08-06 20:16:08 +0200

    Componente respuesta.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PreguntaComponent } from './pregunta/pregunta.component';
+import { RespuestaComponent } from './respuesta/respuesta.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PreguntaComponent
+    PreguntaComponent,
+    RespuestaComponent
   ],
   imports: [
     BrowserModule
